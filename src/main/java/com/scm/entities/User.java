f89df631dfa767fda4f0262d4bb0cd6acf047d2d
siddharthkt45 <tiwarisk1471@gmail.com>
2025-01-11 01:21:07 +0530

    Added entity classes for User, Contact, and SocialLink with JPA mappings

---
+++ src/main/java/com/scm/entities/User.java
@@ -0,0 +1,53 @@
+package com.scm.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    private String userId;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String password;
+    @Column(length = 5000)
+    private String about;
+    @Column(length = 5000)
+    private String profilePic;
+    private String phoneNumber;
+    
+    private boolean enabled = false;
+    private boolean emailVerified = false;
+    private boolean phoneVerified = false;
+
+    // SELF, GOOGLE, GITHUB
+    private Providers provider = Providers.SELF;
+    private String providerUserId;
+
+    // add more fields if needed
+
+    // mapping
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Contact> contacts = new ArrayList<>();
+    
+}
